e0d4374f8454c0b920fd4086bb065a281b3d011c
HOLLY <775363056@qq.com>
2022-04-23 01:31:10 +0800

    修改

--- Front-end-application/web-app/src/views/HomeView.vue
+++ Front-end-application/web-app/src/views/HomeView.vue
@@ -3,13 +3,10 @@
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
 
   export default {
-    name: 'HomeView',
 
     components: {
-      HelloWorld,
     },
   }
 </script>
