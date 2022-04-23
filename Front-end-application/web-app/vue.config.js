4bf30809f748a0de1ece04cdd0516c3454dae193
HOLLY <775363056@qq.com>
2022-04-23 21:55:55 +0800

    PWA

--- Front-end-application/web-app/vue.config.js
+++ Front-end-application/web-app/vue.config.js
@@ -3,5 +3,18 @@ module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
+  pwa: {
+    name: 'test',
+    themeColor: '#4c89fe',
+    msTileColor: '#4c89fe',
+    manifestOptions: {
+      start_url: '.',
+      background_color: '#4c89fe'
+    },
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+    }
+  },
+  // 名字和颜色涉及添加至桌面的应用名，及桌面进入的启动页面的样式
   lintOnSave: true
 })
